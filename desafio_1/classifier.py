93d4de4cc8943547bbd8a488238ab3daeeb9493f
wglastonio <wglastonio@gmail.com>
2025-02-24 22:18:54 -0300

    "Logic change"

--- desafio_1/classifier.py
+++ desafio_1/classifier.py
@@ -1,23 +1,19 @@
-
-hero = []
 hero_name = input('What is the hero name? ')
-hero.append(hero_name)
 hero_xp = int(input('What is the hero XP? '))
-hero.append(hero_xp)
 
-if (hero[1] > 10000):
+if (hero_xp > 10000):
     level = "Radiante";
-elif (hero[1] > 9000):
+elif (hero_xp > 9000):
     level = "Imortal";
-elif (hero[1] > 8000):
+elif (hero_xp > 8000):
     level = "Ascendente";
-elif (hero[1] > 7000):
+elif (hero_xp > 7000):
     level = "Platina";
-elif (hero[1] > 5000):
+elif (hero_xp > 5000):
     level = "Ouro";
-elif (hero[1] > 2000):
+elif (hero_xp > 2000):
     level = "Prata";
-elif (hero[1] > 1000):
+elif (hero_xp > 1000):
     level = "Bronze";
 else:
     level = "Ferro";
